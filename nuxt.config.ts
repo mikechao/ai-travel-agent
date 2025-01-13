d83811e21d799424dc6d5af5fee38ab1cb335bb2
mikechao <mike.chao.one@gmail.com>
2025-01-13 12:06:45 -0800

    Setup open ai api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      // for now leave on public for client side demo of langchain examples
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY
+    }
+  }
 })
