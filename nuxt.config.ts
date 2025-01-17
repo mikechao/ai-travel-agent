27d34d519fddb7044c9ab3e27fb803aee11485ee
mikechao <mike.chao.one@gmail.com>
2025-01-16 22:04:56 -0800

    Added @nuxtjs/tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   runtimeConfig: {
     openaiAPIKey: ''
-  }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
