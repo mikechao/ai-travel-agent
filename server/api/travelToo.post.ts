8af9b8f2d9b3c8c52dc93b327c2a138b25279c48
mikechao <mike.chao.one@gmail.com>
2025-01-19 19:09:25 -0800

    Trying to implement multi agent collab with human

--- server/api/travelToo.post.ts
+++ server/api/travelToo.post.ts
@@ -1,12 +1,19 @@
 import { z } from "zod"
-import { DynamicStructuredTool } from '@langchain/core/tools'
-import { Command } from '@langchain/langgraph'
+import { DynamicStructuredTool, StructuredTool, tool } from '@langchain/core/tools'
+import { Annotation, Command, END, interrupt, MessagesAnnotation } from '@langchain/langgraph'
 import { 
   Message as VercelChatMessage, 
   formatDataStreamPart 
 } from 'ai'
+import { ToolNode } from "@langchain/langgraph/prebuilt"
+import { ChatOpenAI } from "@langchain/openai"
+import { PostgresSaver } from "@langchain/langgraph-checkpoint-postgres"
+import { AIMessage, BaseMessage, ToolMessage } from "@langchain/core/messages"
+import { Runnable } from "@langchain/core/runnables"
+import { convertToOpenAITool } from "@langchain/core/utils/function_calling"
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts"
 
-export default defineLazyEventHandler(() => {
+export default defineLazyEventHandler(async () => {
   const runtimeConfig = useRuntimeConfig()
 
   const weatherForecastTool = new DynamicStructuredTool({
@@ -26,6 +33,85 @@ export default defineLazyEventHandler(() => {
       return JSON.stringify(forecast)
     }
   })
+
+  const askHumanTool = tool((_) => {
+    const userInput = interrupt('ready for user input')
+    return userInput
+  }, {
+    name: "askHuman",
+    description: "Ask the human for input.",
+    schema: z.string(),
+  });
+
+  const toolNode = new ToolNode([weatherForecastTool])
+
+  const model = new ChatOpenAI({
+    model: 'gpt-4o-mini',
+    temperature: 0.6,
+    apiKey: runtimeConfig.openaiAPIKey,
+  })
+
+  const modelWithTools = model.bindTools([weatherForecastTool])
+  
+  const checkpointer = PostgresSaver.fromConnString(
+    "postgresql://postgres:postgres@127.0.0.1:54322/postgres"
+  );
+  await checkpointer.setup()
+
+  const AgentState = Annotation.Root({
+    messages: Annotation<BaseMessage[]>({
+      reducer: (x, y) => x.concat(y),
+    }),
+    sender: Annotation<string>({
+      reducer: (x, y) => y ?? x ?? "user",
+      default: () => "user",
+    }),
+  })
+
+  async function createAgent({
+    llm,
+    tools,
+    systemMessage,
+  }: {
+    llm: ChatOpenAI;
+    tools: StructuredTool[];
+    systemMessage: string;
+  }): Promise<Runnable> {
+    const toolNames = tools.map((tool) => tool.name).join(", ");
+    const formattedTools = tools.map((t) => convertToOpenAITool(t));
+  
+    let prompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "You are a helpful AI assistant, collaborating with other assistants." +
+        " Use the provided tools to progress towards answering the question." +
+        " You have access to the following tools: {tool_names}.\n{system_message}",
+      ],
+      new MessagesPlaceholder("messages"),
+    ]);
+    prompt = await prompt.partial({
+      system_message: systemMessage,
+      tool_names: toolNames,
+    });
+  
+    return prompt.pipe(llm.bind({ tools: formattedTools }));
+  }
+
+  async function callModel(state: typeof MessagesAnnotation.State): Promise<Partial<typeof MessagesAnnotation.State>> {
+    console.log('callModel called')
+    const messages = state.messages;
+    const response = await modelWithTools.invoke(messages);
+    if (response.content && response.content.length > 0) {
+      console.log("response.content", response.content)
+    } else if (response.tool_calls && response.tool_calls.length > 0) {
+      console.log('response.tool_calls[0].name', response.tool_calls[0].name)
+      console.log('response.tool_calls[0].args', response.tool_calls[0].args)
+    } else {
+      console.dir(response)
+    }
+    // We return an object with a messages property, because this will get added to the existing list
+    return { messages: [response] };
+  }
 
   return defineEventHandler(async (webEvent) => {
     const body = await readBody(webEvent)
