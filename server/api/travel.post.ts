7cc25cbc2247e705fa3776dd08067e37c5c006cd
mikechao <mike.chao.one@gmail.com>
2025-01-20 19:24:26 -0800

    Extracted out weather search tool

--- server/api/travel.post.ts
+++ server/api/travel.post.ts
@@ -29,6 +29,7 @@ import {
   MessagesPlaceholder 
 } from "@langchain/core/prompts";
 import { getHotelSearchTool, } from "../utils/hotelSearchTool";
+import { getWeatherForecastTool } from "../utils/weatherSearchTool";
 
 export default defineLazyEventHandler(async () => {
 const runtimeConfig = useRuntimeConfig()
@@ -38,23 +39,7 @@ const toolTag = 'tool-out'
 
 type AIMsg = { role: string; content: string; name: string; toolsToCall?: string }
 
-const weatherForecastTool = new DynamicStructuredTool({
-  name: 'weatherForecastTool',
-  description: 'Use to forecast the weather for the location the user has expressed an interest in',
-  schema: z.object({
-    lat : z.number().describe('The Latitude in decimal degree of the location to get forecast for'),
-    long: z.number().describe('The Longitude in decimal degree of the location to get forecast for')
-  }),
-  func: async (input: { lat: number; long: number }) => {
-    const { lat, long } = input;
-    console.log('weatherForecastTool callled!!!!')
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${runtimeConfig.weatherAPIKey}&q=${lat},${long}&days=7&aqi=no&alerts=no`
-    console.log('url', url)
-    const forecast = await $fetch(url)
-    // comes back as object, JSON.stringify it since it will be stored in ToolMessage.content
-    return JSON.stringify(forecast)
-  }
-})
+const weatherForecastTool = getWeatherForecastTool()
 
 const hotelSearchTool = getHotelSearchTool()
 
