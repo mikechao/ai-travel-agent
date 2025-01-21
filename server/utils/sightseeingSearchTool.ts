9b8b229b49f941a2b4750bfa3fd8a90a491b371c
mikechao <mike.chao.one@gmail.com>
2025-01-20 21:45:17 -0800

    Added sightseeingSearchTool

---
+++ server/utils/sightseeingSearchTool.ts
@@ -0,0 +1,53 @@
+import { DynamicStructuredTool } from "@langchain/core/tools"
+import { z } from "zod"
+import { URL } from 'url'
+
+const runtimeConfig = useRuntimeConfig()
+
+interface LocationSearchResult {
+  data: any[],
+  error: any
+}
+
+const sightseeingSearchTool = new DynamicStructuredTool({
+  name: 'sightseeingSearchTool',
+  description: `Used to search for sightseeing locations that the user is interested in. 
+   Data provided by Tripadvisor Content API`,
+  schema: z.object({
+    lat : z.number().describe('The Latitude in decimal degree of the location to search for sightseeing locations'),
+    long: z.number().describe('The Longitude in decimal degree of the location to search for sightseeing locations'),
+    searchQuery: z.string().describe('Text to use for searching based on the name of the location'),
+    radius: z.number().optional()
+      .default(10)
+      .describe(`Length of the radius in miles from the provided lat, long pair to filter results.`)
+  }),
+  func: async ({ lat, long, searchQuery, radius = 10 }: { lat: number; long: number; searchQuery: string; radius?: number }) => {
+    console.log(`sightseeingSearchTool called with lat: ${lat}, long: ${long}, searchQuery: ${searchQuery}, radius: ${radius}`)
+    const sightseeingURL = new URL('https://api.content.tripadvisor.com/api/v1/location/search')
+    sightseeingURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
+    sightseeingURL.searchParams.set('searchQuery', searchQuery)
+    sightseeingURL.searchParams.set('category', 'attractions')
+    sightseeingURL.searchParams.set('latLong', `${lat},${long}`)
+    sightseeingURL.searchParams.set('radius', radius.toString())
+    sightseeingURL.searchParams.set('radiusUnit', 'mi')
+    console.log('sightseeingURL', sightseeingURL.toString())
+
+    const {data , error} = await $fetch<LocationSearchResult>(sightseeingURL.toString(), {
+      method: 'GET',
+      headers: {
+        'accept': 'application/json'
+      }
+    })
+    if (error) {
+      console.error('error fetching sightseeing data', error)
+      // need to figure out how to handle errors in tools for langchain
+    }
+    if (!data.length) {
+      // not sure if this the right way to handle this
+      return "No sights found"
+    }
+    return JSON.stringify(data)
+  }
+})
+
+export const getSightseeingSearchTool = () => sightseeingSearchTool
