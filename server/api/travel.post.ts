77f46536c704c1a0ac05c4ab9140a8337770ea54
mikechao <mike.chao.one@gmail.com>
2025-01-20 17:54:51 -0800

    Setting up the hotelSearchTool

--- server/api/travel.post.ts
+++ server/api/travel.post.ts
@@ -55,6 +55,25 @@ const weatherForecastTool = new DynamicStructuredTool({
   }
 })
 
+const hotelSearchTool = new DynamicStructuredTool({
+  name: 'hotelSearchTool',
+  description: 'Used to search for hotels for the location the user has expressed an interest in',
+  schema: z.object({
+    lat : z.number().describe('The Latitude in decimal degree of the location to search for hotels'),
+    long: z.number().describe('The Longitude in decimal degree of the location to search for hotelsr'),
+    searchQuery: z.string().describe('Text to use for searching based on the name of the location'),
+    radius: z.number().optional()
+      .default(10)
+      .describe(`Length of the radius in miles from the provided lat, long pair to filter results.`)
+  }),
+  func: async (input: { lat: number; long: number; searchQuery: string; radius?: number }) => {
+    const { lat, long, searchQuery,  radius} = input
+    console.log(`hotelSearchTool called with lat: ${lat}, long: ${long}, searchQuery: ${searchQuery}, radius: ${radius}`)
+
+    return "tool resposne"
+  }
+})
+
 const toolsByName = new Map<string, StructuredToolInterface>()
 toolsByName.set(weatherForecastTool.name, weatherForecastTool)
 
