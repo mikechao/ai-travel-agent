1a7ac2f52dcb46a46e02922e0d18096e92e2a2e9
mikechao <mike.chao.one@gmail.com>
2025-01-22 11:27:15 -0800

    Lint fix

--- components/weather/BorderLine.vue
+++ components/weather/BorderLine.vue
@@ -1,4 +1,4 @@
 <template>
   <!-- border -->
-  <div class="w-full h-px bg-gradient-to-r from-white/0 via-white/90 to-white/0 my-10"></div>
+  <div class="w-full h-px bg-gradient-to-r from-white/0 via-white/90 to-white/0 my-10" />
 </template>
