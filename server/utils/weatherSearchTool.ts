42ef549b5044af3f013518df4974a18d95b08326
mikechao <mike.chao.one@gmail.com>
2025-01-22 23:47:43 -0800

    Use consola to log and lint fixes

--- server/utils/weatherSearchTool.ts
+++ server/utils/weatherSearchTool.ts
@@ -1,5 +1,6 @@
-import { DynamicStructuredTool } from "@langchain/core/tools"
-import { z } from "zod"
+import { DynamicStructuredTool } from '@langchain/core/tools'
+import { consola } from 'consola'
+import { z } from 'zod'
 
 const runtimeConfig = useRuntimeConfig()
 
@@ -7,18 +8,18 @@ const weatherForecastTool = new DynamicStructuredTool({
   name: 'weatherForecastTool',
   description: 'Use to forecast the weather for the location the user has expressed an interest in',
   schema: z.object({
-    lat : z.number().describe('The Latitude in decimal degree of the location to get forecast for'),
-    long: z.number().describe('The Longitude in decimal degree of the location to get forecast for')
+    lat: z.number().describe('The Latitude in decimal degree of the location to get forecast for'),
+    long: z.number().describe('The Longitude in decimal degree of the location to get forecast for'),
   }),
-  func: async (input: { lat: number; long: number }) => {
-    const { lat, long } = input;
-    console.log('weatherForecastTool callled!!!!')
+  func: async (input: { lat: number, long: number }) => {
+    const { lat, long } = input
+    consola.info('weatherForecastTool callled!!!!')
     const url = `http://api.weatherapi.com/v1/forecast.json?key=${runtimeConfig.weatherAPIKey}&q=${lat},${long}&days=7&aqi=no&alerts=no`
-    console.log('url', url)
+    consola.info('url', url)
     const forecast = await $fetch(url)
     // comes back as object, JSON.stringify it since it will be stored in ToolMessage.content
     return JSON.stringify(forecast)
-  }
+  },
 })
 
 export const getWeatherForecastTool = () => weatherForecastTool
