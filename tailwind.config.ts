8c9d7fd652ed21d16d67ca896a0e87773e97fb74
mikechao <mike.chao.one@gmail.com>
2025-01-22 23:20:34 -0800

    Adding custom tailwindcss for weather card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,16 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-day': '#8ec5fc',
+        'bg-night': '#07223d',
+      },
+      backgroundImage: {
+        'bg-day-backgrounImage': 'linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%)',
+        'bg-night-backgroundImage': 'linear-gradient(62deg, #0a2a4a 0%, #270845 100%)',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
-
