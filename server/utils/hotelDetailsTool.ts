062d81b41769ce9723f13f17009406b4afad9bc8
mikechao <mike.chao.one@gmail.com>
2025-01-23 09:47:31 -0800

    Use consola to log and lint fixes

--- server/utils/hotelDetailsTool.ts
+++ server/utils/hotelDetailsTool.ts
@@ -1,6 +1,7 @@
-import { DynamicStructuredTool } from "@langchain/core/tools"
-import { z } from "zod"
-import { URL } from 'url'
+import { URL } from 'node:url'
+import { DynamicStructuredTool } from '@langchain/core/tools'
+import { consola } from 'consola'
+import { z } from 'zod'
 
 const runtimeConfig = useRuntimeConfig()
 
@@ -8,28 +9,28 @@ const hotelDetailsTool = new DynamicStructuredTool({
   name: 'hotelDetailsTool',
   description: 'Used to get additional details about a hotel',
   schema: z.object({
-    locationId: z.string().describe('The location_id from previous results of the tool named hotelSearchTool')
+    locationId: z.string().describe('The location_id from previous results of the tool named hotelSearchTool'),
   }),
   func: async ({ locationId }: { locationId: string }) => {
-    console.log(`hotelDetailsTool called with locationId: ${locationId}`)
+    consola.info(`hotelDetailsTool called with locationId: ${locationId}`)
     const locationDetailsURL = new URL(`https://api.content.tripadvisor.com/api/v1/location/${locationId}/details`)
-    locationDetailsURL.searchParams.set('key',`${runtimeConfig.tripAdvisorAPIKey}`)
-    console.log('locationDetailsURL', locationDetailsURL.toString())
+    locationDetailsURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
+    consola.info('locationDetailsURL', locationDetailsURL.toString())
 
     try {
       const response = await $fetch(locationDetailsURL.toString(), {
         method: 'GET',
         headers: {
-          'accept': 'application/json'
-        }
+          accept: 'application/json',
+        },
       })
       return JSON.stringify(response)
-    } catch (error) {
+    }
+    catch (error) {
       console.error('error fetching location details', error)
       return 'Error fetching location details'
     }
-
-  }
+  },
 })
 
 export const getHotelDetailsTool = () => hotelDetailsTool
