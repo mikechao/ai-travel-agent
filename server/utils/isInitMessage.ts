8d39d2ccb2659f9c33e7854669f4a869a265b1f8
mikechao <mike.chao.one@gmail.com>
2025-01-22 23:32:26 -0800

    Fixes according to eslint

--- server/utils/isInitMessage.ts
+++ server/utils/isInitMessage.ts
@@ -1,11 +1,13 @@
-import {Message as VercelChatMessage } from "ai"
+import type { Message as VercelChatMessage } from 'ai'
 
 export function isInitMessage(message: VercelChatMessage) {
   if (message.data) {
     try {
       const initData = JSON.parse(message.data as string)
-      return initData.hasOwnProperty('init') && initData['init']
-    } catch (error) {
+      return Object.prototype.hasOwnProperty.call(initData, 'init') && initData.init
+    }
+    catch (error) {
+      console.error('Error parsing message.data', error)
       return false
     }
   }
