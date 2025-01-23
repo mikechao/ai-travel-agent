8d5e92b179e56cdfff99c5147e0fe40e14af7e5c
mikechao <mike.chao.one@gmail.com>
2025-01-23 09:50:50 -0800

    Switch to consola and lint fixes

--- server/utils/sightseeingSearchTool.ts
+++ server/utils/sightseeingSearchTool.ts
@@ -1,11 +1,12 @@
-import { DynamicStructuredTool } from "@langchain/core/tools"
-import { z } from "zod"
-import { URL } from 'url'
+import { URL } from 'node:url'
+import { DynamicStructuredTool } from '@langchain/core/tools'
+import { consola } from 'consola'
+import { z } from 'zod'
 
 const runtimeConfig = useRuntimeConfig()
 
 interface LocationSearchResult {
-  data: any[],
+  data: any[]
   error: any
 }
 
@@ -14,15 +15,13 @@ const sightseeingSearchTool = new DynamicStructuredTool({
   description: `Used to search for sightseeing locations that the user is interested in. 
    Data provided by Tripadvisor Content API`,
   schema: z.object({
-    lat : z.number().describe('The Latitude in decimal degree of the location to search for sightseeing locations'),
+    lat: z.number().describe('The Latitude in decimal degree of the location to search for sightseeing locations'),
     long: z.number().describe('The Longitude in decimal degree of the location to search for sightseeing locations'),
     searchQuery: z.string().describe('Text to use for searching based on the name of the location'),
-    radius: z.number().optional()
-      .default(10)
-      .describe(`Length of the radius in miles from the provided lat, long pair to filter results.`)
+    radius: z.number().optional().default(10).describe(`Length of the radius in miles from the provided lat, long pair to filter results.`),
   }),
-  func: async ({ lat, long, searchQuery, radius = 10 }: { lat: number; long: number; searchQuery: string; radius?: number }) => {
-    console.log(`sightseeingSearchTool called with lat: ${lat}, long: ${long}, searchQuery: ${searchQuery}, radius: ${radius}`)
+  func: async ({ lat, long, searchQuery, radius = 10 }: { lat: number, long: number, searchQuery: string, radius?: number }) => {
+    consola.info(`sightseeingSearchTool called with lat: ${lat}, long: ${long}, searchQuery: ${searchQuery}, radius: ${radius}`)
     const sightseeingURL = new URL('https://api.content.tripadvisor.com/api/v1/location/search')
     sightseeingURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
     sightseeingURL.searchParams.set('searchQuery', searchQuery)
@@ -30,13 +29,13 @@ const sightseeingSearchTool = new DynamicStructuredTool({
     sightseeingURL.searchParams.set('latLong', `${lat},${long}`)
     sightseeingURL.searchParams.set('radius', radius.toString())
     sightseeingURL.searchParams.set('radiusUnit', 'mi')
-    console.log('sightseeingURL', sightseeingURL.toString())
+    consola.info('sightseeingURL', sightseeingURL.toString())
 
-    const {data , error} = await $fetch<LocationSearchResult>(sightseeingURL.toString(), {
+    const { data, error } = await $fetch<LocationSearchResult>(sightseeingURL.toString(), {
       method: 'GET',
       headers: {
-        'accept': 'application/json'
-      }
+        accept: 'application/json',
+      },
     })
     if (error) {
       console.error('error fetching sightseeing data', error)
@@ -44,10 +43,10 @@ const sightseeingSearchTool = new DynamicStructuredTool({
     }
     if (!data.length) {
       // not sure if this the right way to handle this
-      return "No sights found"
+      return 'No sights found'
     }
     return JSON.stringify(data)
-  }
+  },
 })
 
 export const getSightseeingSearchTool = () => sightseeingSearchTool
