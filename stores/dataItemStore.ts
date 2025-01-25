81cdbb86863427077a3274b88b64ead540a28ffc
mikechao <mike.chao.one@gmail.com>
2025-01-25 15:49:29 -0800

    Added return to add/remove

--- stores/dataItemStore.ts
+++ stores/dataItemStore.ts
@@ -3,18 +3,22 @@ export const useDataItemStore = defineStore('dataItem', () => {
 
   const addedDataItemIds = new Set()
 
-  function add(dataItem: DataItem) {
+  function add(dataItem: DataItem): boolean {
     if (!addedDataItemIds.has(dataItem.id)) {
       dataItems.value.push(dataItem)
       addedDataItemIds.add(dataItem.id)
+      return true
     }
+    return false
   }
 
-  function remove(id: string) {
+  function remove(id: string): boolean {
     if (addedDataItemIds.has(id)) {
       addedDataItemIds.delete(id)
       dataItems.value = dataItems.value.filter(item => item.id !== id)
+      return true
     }
+    return false
   }
 
   return {
