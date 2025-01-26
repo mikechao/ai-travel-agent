3de818eb9cfa5520e84a1787382ba584f0cac891
mikechao <mike.chao.one@gmail.com>
2025-01-25 16:30:52 -0800

    Display raw data in ResultsPanel

--- components/ResultsPanel.vue
+++ components/ResultsPanel.vue
@@ -1,3 +1,19 @@
+<!-- eslint-disable no-console -->
+<script setup lang="ts">
+const dataItemStore = useDataItemStore()
+const { dataItems } = storeToRefs(dataItemStore)
+
+watch(dataItems, (newDataItems) => {
+  console.log('got newDataItems in ResultsPanel', newDataItems)
+})
+</script>
+
 <template>
   <h1>Show some UI results here</h1>
+  <div v-if="dataItems">
+    <div v-for="(item, index) of dataItems" :key="index">
+      {{ item }}
+      {{ index }}
+    </div>
+  </div>
 </template>
