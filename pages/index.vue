4a9361f40522c78af07282a544b56bebd9a58e0e
mikechao <mike.chao.one@gmail.com>
2025-01-25 18:58:56 -0800

    Set initial size of 50/50 for ResultsPanel and ChatComponent

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@ import SplitterPanel from 'primevue/splitterpanel'
 
 <template>
   <Splitter style="height: max-content;">
-    <SplitterPanel class="flex items-center justify-center">
+    <SplitterPanel class="flex items-center justify-center" :size="50">
       <ResultsPanel class="h-max" />
     </SplitterPanel>
-    <SplitterPanel class="flex items-center justify-center">
+    <SplitterPanel class="flex items-center justify-center" :size="50">
       <ChatComponent class="h-max" />
     </SplitterPanel>
   </Splitter>
