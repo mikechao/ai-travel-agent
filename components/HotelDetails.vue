d61f6507d44ae825a085333f60a0c62a1a611c36
mikechao <mike.chao.one@gmail.com>
2025-01-27 12:49:54 -0800

    Starting component to show Hotel Details

---
+++ components/HotelDetails.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+export interface Props {
+  hotel: Hotel
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="w-[700px] bg-white shadow-md rounded-lg flex">
+    <div class="w-[250px] h-[200px]">
+      <img src="https://tools-api.webcrumbs.org/image-placeholder/250/200/city/1" alt="NYC Skyline" class="w-[250px] h-[200px] object-cover rounded-l-lg">
+    </div>
+  </div>
+</template>
