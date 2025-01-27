e461af4a805facda7093c3ca8b276abf0f86e5d5
mikechao <mike.chao.one@gmail.com>
2025-01-26 17:50:18 -0800

    Added types matching tripadvisor api call

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,4 +6,22 @@ declare global {
     type: string
     data: string
   }
+
+  interface Address {
+    street1: string
+    street2?: string
+    city: string
+    state: string
+    country: string
+    postalcode: string
+    address_string: string
+  }
+
+  interface Location {
+    location_id: string
+    name: string
+    distance: string
+    bearing: string
+    address_obj: Address
+  }
 }
