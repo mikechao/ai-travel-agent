434efa96b8453502f270c023d816180c85278aba
mikechao <mike.chao.one@gmail.com>
2025-01-28 12:09:52 -0800

    Fix incorrect call in details.get.ts

--- server/api/location/details.get.ts
+++ server/api/location/details.get.ts
@@ -3,11 +3,6 @@ import consola from 'consola'
 export default defineLazyEventHandler(() => {
   const runtimeConfig = useRuntimeConfig()
 
-  interface DetailsResult {
-    data: any
-    error: any
-  }
-
   return defineEventHandler(async (event) => {
     const { locationId } = getQuery(event)
     if (!locationId) {
@@ -20,15 +15,14 @@ export default defineLazyEventHandler(() => {
     const locationDetailsURL = new URL(`https://api.content.tripadvisor.com/api/v1/location/${locationId}/details`)
     locationDetailsURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
 
-    const { data, error } = await $fetch<DetailsResult>(locationDetailsURL.toString())
-    if (error) {
+    try {
+      const response = await $fetch(locationDetailsURL.toString(), { method: 'GET' })
+      return response
+    }
+    catch (error) {
       consola.error('error fetching details', error)
       setResponseStatus(event, 500, 'Error gettings details')
       return 'Unexpected error getting details'
     }
-    if (!data) {
-      return sendNoContent(event, 204)
-    }
-    return data
   })
 })
