56c1a310872da68fbbaafb4a399859483ed27771
mikechao <mike.chao.one@gmail.com>
2025-01-28 11:42:13 -0800

    Fixed typo in error message

--- server/api/photos.get.ts
+++ server/api/photos.get.ts
@@ -2,12 +2,12 @@ import { URL } from 'node:url'
 import { consola } from 'consola'
 
 export default defineLazyEventHandler(() => {
-const runtimeConfig = useRuntimeConfig()
+  const runtimeConfig = useRuntimeConfig()
 
-interface PhotoSearchResult {
-  data: any[]
-  error: any
-}
+  interface PhotoSearchResult {
+    data: any[]
+    error: any
+  }
 
   return defineEventHandler(async (event) => {
     const { locationId } = getQuery(event)
@@ -15,21 +15,21 @@ interface PhotoSearchResult {
       consola.error('Get photos request failed, event missing locationId param', JSON.stringify(event))
       throw createError({
         statusCode: 400,
-        statusMessage: 'Request is missing uid param'
+        statusMessage: 'Request is missing locationId param',
       })
     }
     const photoURL = new URL(`https://api.content.tripadvisor.com/api/v1/location/${locationId}/photos`)
     photoURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
 
     const { data, error } = await $fetch<PhotoSearchResult>(photoURL.toString(), {
-      method: 'GET'
+      method: 'GET',
     })
     if (error) {
       consola.error('error fetching photos', error)
       setResponseStatus(event, 500, 'Error gettings photos')
-      return "Unexpected error getting photos"
+      return 'Unexpected error getting photos'
     }
-    if (!data || ! data.length) {
+    if (!data || !data.length) {
       return sendNoContent(event, 204)
     }
 
