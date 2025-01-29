ebd3ac0047a5a52353db7962e5a27702d9ff64ac
mikechao <mike.chao.one@gmail.com>
2025-01-29 14:01:18 -0800

    Updated test endpoint to stream some text

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,11 +1,46 @@
-// just a simple test endpoint that will return
-// a message that contains some stuff in message.annotations
+// just a simple test endpoint that will return some text
 export default defineLazyEventHandler(async () => {
+  const text1 = `
+Two roads diverged in a yellow wood,<br/>
+And sorry I could not travel both<br/>
+And be one traveler, long I stood<br/>
+And looked down one as far as I could<br/>
+To where it bent in the undergrowth;<br/>
+<br/>
+The poem should be above weather data.<br/>
+<br/>
+`
+
+  const text2 = `
+<br/>
+The weather data should be above this poem<br/>
+Is this the last time I’ll admire the guys<br/>
+in their neon-yellow slickers, guiding us<br/>
+to our parking spots before we head up<br/>
+ <br/>
+two flights to the passenger deck,<br/>
+to the cafeteria where a man in a black derby<br/>
+and black suspenders nods and smiles<br/>
+ <br/>
+`
+  function delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
   return defineEventHandler(async (_webEvent) => {
     return new ReadableStream({
       async start(controller) {
-        controller.enqueue(`8:[{"type":"weather"}]\n`)
-        controller.close()
+        for (const chunk of text1.split(' ')) {
+          // simluate some delay
+          await delay(50)
+          controller.enqueue(`${chunk} `)
+        }
+        for (const chunk of text2.split(' ')) {
+          // simluate some delay
+          await delay(50)
+          controller.enqueue(`${chunk} `)
+        }
+        controller.close() // Close the stream when done
       },
     })
   })
