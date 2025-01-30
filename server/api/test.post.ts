454524a412bef8c2f942b20c27af2ab602fb75f8
mikechao <mike.chao.one@gmail.com>
2025-01-29 16:21:50 -0800

    Have the test end point return hotel and sights lists

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -3,6 +3,7 @@ import consola from 'consola'
 import { v4 as uuidv4 } from 'uuid'
 
 // just a simple test endpoint that will return some text
+// or data based on certain content in the message
 export default defineLazyEventHandler(async () => {
   const weatherData = JSON.stringify({
     location: {
@@ -184,6 +185,300 @@ export default defineLazyEventHandler(async () => {
     },
   })
 
+  const sightsData = JSON.stringify([
+    {
+      location_id: '15266827',
+      name: 'AR Workshop Danville',
+      distance: '0.057751323328638283',
+      bearing: 'south',
+      address_obj: {
+        street1: '355 Hartz Ave',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3337',
+        address_string: '355 Hartz Ave, Danville, CA 94526-3337',
+      },
+    },
+    {
+      location_id: '10533214',
+      name: 'Danville Massage',
+      distance: '0.08893569689731762',
+      bearing: 'north',
+      address_obj: {
+        street1: '199 E Linda Mesa Ave',
+        street2: 'Suite A',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3339',
+        address_string: '199 E Linda Mesa Ave Suite A, Danville, CA 94526-3339',
+      },
+    },
+    {
+      location_id: '6542005',
+      name: 'Museum of the San Ramon Valley',
+      distance: '0.12036769615105866',
+      bearing: 'south',
+      address_obj: {
+        street1: '205 Railroad Ave',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3892',
+        address_string: '205 Railroad Ave, Danville, CA 94526-3892',
+      },
+    },
+    {
+      location_id: '7222647',
+      name: 'Auburn James Winery',
+      distance: '0.028748811975822887',
+      bearing: 'south',
+      address_obj: {
+        street1: '321 Hartz Ave',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3336',
+        address_string: '321 Hartz Ave, Danville, CA 94526-3336',
+      },
+    },
+    {
+      location_id: '12555104',
+      name: 'St. Isidore Catholic Church',
+      distance: '0.3637733037222501',
+      bearing: 'northwest',
+      address_obj: {
+        street1: '440 La Gonda Way',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-2562',
+        address_string: '440 La Gonda Way, Danville, CA 94526-2562',
+      },
+    },
+    {
+      location_id: '23183728',
+      name: 'Good Life Spa',
+      distance: '0.16436203744151404',
+      bearing: 'west',
+      address_obj: {
+        street1: '158 W Linda Mesa Ave',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3333',
+        address_string: '158 W Linda Mesa Ave, Danville, CA 94526-3333',
+      },
+    },
+    {
+      location_id: '17780940',
+      name: 'San Damiano Retreat',
+      distance: '1.0645003893283982',
+      bearing: 'southwest',
+      address_obj: {
+        street1: '710 Highland Dr',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3704',
+        address_string: '710 Highland Dr, Danville, CA 94526-3704',
+      },
+    },
+    {
+      location_id: '12922261',
+      name: 'Hand & Stone Massage and Facial Spa',
+      distance: '0.20422414711687198',
+      bearing: 'northwest',
+      address_obj: {
+        street1: '11 Railroad Ave',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3873',
+        address_string: '11 Railroad Ave, Danville, CA 94526-3873',
+      },
+    },
+    {
+      location_id: '2475305',
+      name: 'Front Street Cottage Inc',
+      distance: '0.1802254437961061',
+      bearing: 'east',
+      address_obj: {
+        street1: '212 Front St',
+        street2: '',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526-3402',
+        address_string: '212 Front St, Danville, CA 94526-3402',
+      },
+    },
+    {
+      location_id: '117180',
+      name: 'Iron Horse Trail',
+      distance: '0.2997450420014402',
+      bearing: 'west',
+      address_obj: {
+        city: 'Dublin',
+        state: 'California',
+        country: 'United States',
+        address_string: 'Dublin, CA',
+      },
+    },
+  ])
+
+  const hotelsData = JSON.stringify([
+    {
+      location_id: '617187',
+      name: 'Best Western Danville Sycamore Inn',
+      distance: '0.894566366267708',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '803 Camino Ramon',
+        street2: '',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94526',
+        address_string: '803 Camino Ramon, Danville, CA 94526',
+      },
+    },
+    {
+      location_id: '235451',
+      name: 'Extended Stay America - San Ramon - Bishop Ranch - East',
+      distance: '3.2039439427959677',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '2100 Camino Ramon',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583-1362',
+        address_string: '2100 Camino Ramon, San Ramon, CA 94583-1362',
+      },
+    },
+    {
+      location_id: '250131',
+      name: 'HYATT house San Ramon',
+      distance: '3.233720267744259',
+      bearing: 'south',
+      address_obj: {
+        street1: '2323 San Ramon Valley Blvd',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583-1607',
+        address_string: '2323 San Ramon Valley Blvd, San Ramon, CA 94583-1607',
+      },
+    },
+    {
+      location_id: '1147338',
+      name: 'National at Pinnacle Crow Canyo',
+      distance: '3.22589942308101',
+      bearing: 'south',
+      address_obj: {
+        street1: '1700 Promontory Ter',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583-1580',
+        address_string: '1700 Promontory Ter, San Ramon, CA 94583-1580',
+      },
+    },
+    {
+      location_id: '20144998',
+      name: 'San Ramon Valley Monthly Vacation Home',
+      distance: '4.434286347565809',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '50 Crestridge Ct',
+        city: 'Danville',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94506-1107',
+        address_string: '50 Crestridge Ct, Danville, CA 94506-1107',
+      },
+    },
+    {
+      location_id: '81779',
+      name: 'San Ramon Marriott',
+      distance: '4.473933962159359',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '2600 Bishop Dr',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583-2338',
+        address_string: '2600 Bishop Dr, San Ramon, CA 94583-2338',
+      },
+    },
+    {
+      location_id: '217300',
+      name: 'Extended Stay America - San Ramon - Bishop Ranch - West',
+      distance: '4.535170248336561',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '18000 San Ramon Valley Blvd',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583-4401',
+        address_string: '18000 San Ramon Valley Blvd, San Ramon, CA 94583-4401',
+      },
+    },
+    {
+      location_id: '80082',
+      name: 'Sonesta Select San Ramon',
+      distance: '4.799962867495464',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '18090 San Ramon Valley Blvd.',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583',
+        address_string: '18090 San Ramon Valley Blvd., San Ramon, CA 94583',
+      },
+    },
+    {
+      location_id: '17656548',
+      name: 'Global Luxury Suites In San Ramon',
+      distance: '4.13654646900972',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '125 Shoreline Cir',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94582-4648',
+        address_string: '125 Shoreline Cir, San Ramon, CA 94582-4648',
+      },
+    },
+    {
+      location_id: '80077',
+      name: 'Residence Inn San Ramon',
+      distance: '4.770957813749616',
+      bearing: 'southeast',
+      address_obj: {
+        street1: '1071 Market Place',
+        street2: '',
+        city: 'San Ramon',
+        state: 'California',
+        country: 'United States',
+        postalcode: '94583',
+        address_string: '1071 Market Place, San Ramon, CA 94583',
+      },
+    },
+  ])
+
   const text1 = `
 Two roads diverged in a yellow wood,<br/>
 And sorry I could not travel both<br/>
@@ -215,11 +510,34 @@ and black suspenders nods and smiles<br/>
     const body = await readBody(webEvent)
     const { messages } = body
     consola.info('messages', messages)
+
     if (messages[0].content === 'weather') {
       const id = uuidv4()
       return new ReadableStream({
         async start(controller) {
           const part = `2:[{"id":"${id}","type":"weather","data":${weatherData}}]\n`
+          controller.enqueue(part)
+          controller.close()
+        },
+      })
+    }
+
+    if (messages[0].content === 'sights') {
+      const id = uuidv4()
+      return new ReadableStream({
+        async start(controller) {
+          const part = `2:[{"id":"${id}","type":"sight-search","data":${sightsData}}]\n`
+          controller.enqueue(part)
+          controller.close()
+        },
+      })
+    }
+
+    if (messages[0].content === 'hotel') {
+      const id = uuidv4()
+      return new ReadableStream({
+        async start(controller) {
+          const part = `2:[{"id":"${id}","type":"hotel-search","data":${hotelsData}}]\n`
           controller.enqueue(part)
           controller.close()
         },
