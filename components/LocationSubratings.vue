535e8106afca218d32af02b9aabebc9cc47f6efe
mikechao <mike.chao.one@gmail.com>
2025-02-01 11:19:52 -0800

    Switch to using table instead of DataTable

--- components/LocationSubratings.vue
+++ components/LocationSubratings.vue
@@ -8,14 +8,26 @@ defineProps({
 </script>
 
 <template>
-  <DataTable :value="subratings" class="w-auto">
-    <Column field="localized_name" header="Name" />
-    <Column header="Rating">
-      <template #body="slotProps">
-        <img
-          :src="slotProps.data.rating_image_url"
-        >
-      </template>
-    </Column>
-  </DataTable>
+  <table class="w-fit divide-y divide-gray-200 bg-surface-0 dark:bg-surface-900 text-surface-700 dark:text-surface-0">
+    <thead class="">
+      <tr>
+        <th class="px-4 pb-3 text-left font-medium uppercase tracking-wider">
+          Name
+        </th>
+        <th class="px-4 pb-3 text-left font-medium uppercase tracking-wider">
+          Rating
+        </th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200">
+      <tr v-for="rating in subratings" :key="rating.name">
+        <td class="px-4 py-3 whitespace-nowrap ">
+          {{ rating.localized_name }}
+        </td>
+        <td class="px-4 py-3 whitespace-nowrap">
+          <img :src="rating.rating_image_url" :alt="rating.localized_name">
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
