121fb6a75dc4d146d24517b75cd35e6c7665569c
mikechao <mike.chao.one@gmail.com>
2025-02-01 11:20:01 -0800

    Remove unused theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,6 @@ import primeui from 'tailwindcss-primeui'
 
 export default {
   content: [],
-  theme: {
-    extend: {
-      colors: {
-        'bg-day': '#8ec5fc',
-        'bg-night': '#07223d',
-      },
-      backgroundImage: {
-        'bg-day-backgrounImage': 'linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%)',
-        'bg-night-backgroundImage': 'linear-gradient(62deg, #0a2a4a 0%, #270845 100%)',
-      },
-    },
-  },
+  theme: {},
   plugins: [primeui],
 } satisfies Config
