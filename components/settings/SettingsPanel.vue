1d47e7843d4d0a9bca63e4d828a712445c5eb79d
mikechao <mike.chao.one@gmail.com>
2025-02-03 10:53:49 -0800

    Renamed to ColorSelection

--- components/settings/SettingsPanel.vue
+++ components/settings/SettingsPanel.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import DarkLightMode from './DarkLightMode.vue'
-import PrimaryColor from './PrimaryColor.vue'
 </script>
 
 <template>
   <DarkLightMode />
   <Divider />
-  <PrimaryColor />
+  <ColorSelection />
 </template>
