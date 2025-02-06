0aa13ef05bdcd1a69418de58d874fdc9ee8d0522
mikechao <mike.chao.one@gmail.com>
2025-02-06 13:02:36 -0800

    Hooked up the TravelRecommendToolKit to travelAdvisor

--- server/api/travel.post.ts
+++ server/api/travel.post.ts
@@ -156,8 +156,14 @@ export default defineLazyEventHandler(async () => {
   async function travelAdvisor(state: typeof AgentState.State): Promise<Command> {
     consola.info('travelAdvisor')
     const systemPrompt
-      = `Your name is Pluto the pup and you are a general travel expert that can recommend travel destinations based on the user's interests 
-         by using the tool \'travelRecommendationTool\' which can take a little bit `
+      = `Your name is Pluto the pup and you are a general travel expert that can recommend travel destinations based on the user's interests `
+        + ` To help you recommend travel destinations based on the user's interests you can use the following tools `
+        + ` Step 1. The \'searchQueryTool\' will generate travel destinations search queries based on the user's interests, present the results of 
+         this tool to the user, wait for the user to pick 1 or more results to explore farther `
+        + ` Step 2. When the user has select 1 or more result from step 1 use the \'searchExecutionTool\' to execute travel destination search queries on the internet,
+          present the results of this tool to the user, wait for the user to pick 1 or more results to explore farther `
+        + ` Step 3. When the user has selected 1 or more results from Step 2 use the \'searchSummaryTool\' to generate a summary related to the travel destination search queries based on the user's interest,
+          present the results of this tool to the user `
         + ` Be sure to bark a lot and use dog related emojis `
         + `If you need sightseeing or attraction recommendations, ask \'sightseeingAdvisor\' named Polly Parrot for help. `
         + 'If you need hotel recommendations, ask \'hotelAdvisor\' named Penny Restmore for help. '
