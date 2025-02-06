a91455ed00ce0162284f5f453f7b49eb3d3292ac
mikechao <mike.chao.one@gmail.com>
2025-02-06 09:51:34 -0800

    Laying out RunnableLambdas to do the samething as TravelRecommendToolKit

---
+++ server/toolkits/RunnableTools.ts
@@ -0,0 +1,69 @@
+import type { EmbeddingsInterface } from '@langchain/core/embeddings'
+import type { BaseChatModel } from '@langchain/core/language_models/chat_models'
+import { RunnableLambda } from '@langchain/core/runnables'
+import { consola } from 'consola'
+
+interface SearchQueryInput {
+  interest: string
+}
+
+interface SearchQueryOutput {
+  queries: string[]
+}
+
+type SearchExecutionInput = SearchQueryOutput
+
+interface QueryAndURL {
+  query: string
+  url: string
+}
+
+interface SearchExecutionOuput {
+  queryAndURLs: QueryAndURL[]
+}
+
+type SearchSummaryInput = SearchExecutionOuput
+
+interface SearchSummaryOutput {
+  results: string[]
+}
+
+export class RunnableTools {
+  llm: BaseChatModel
+  embeddings: EmbeddingsInterface
+
+  constructor(llm: BaseChatModel, embeddings: EmbeddingsInterface) {
+    this.llm = llm
+    this.embeddings = embeddings
+  }
+
+  createSearchQueryRunnable() {
+    return RunnableLambda.from<SearchQueryInput, SearchQueryOutput>((input: SearchQueryInput) => {
+      consola.info('searchQueryRunnable called with ', input.interest)
+      const results = ['best cat cafes in the world', 'cat travel tips']
+      return { queries: results }
+    })
+  }
+
+  createSearchExecutionRunnable() {
+    return RunnableLambda.from<SearchExecutionInput, SearchExecutionOuput>((input: SearchExecutionInput) => {
+      consola.info('searchExecutionRunnable called with ', input.queries)
+
+      const results: QueryAndURL[] = [
+        { query: 'best cat cafes in the world', url: 'https://www.cats.com' },
+        { query: 'cat travel tips', url: 'https://www.cat-travel-tips.com' },
+      ]
+
+      return { queryAndURLs: results }
+    })
+  }
+
+  createSearchSummaryRunnable() {
+    return RunnableLambda.from<SearchSummaryInput, SearchSummaryOutput>((input: SearchSummaryInput) => {
+      consola.info('searchSummaryRunnable called with ', JSON.stringify(input.queryAndURLs))
+      const results: string[] = [`summary 1 ${JSON.stringify(input.queryAndURLs)}`]
+
+      return { results }
+    })
+  }
+}
