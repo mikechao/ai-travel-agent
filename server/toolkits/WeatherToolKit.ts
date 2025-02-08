d6fa198da58f2360540757aea1d769e8b3d75ee7
mikechao <mike.chao.one@gmail.com>
2025-02-08 10:59:14 -0800

    Added a WeatherToolKit

---
+++ server/toolkits/WeatherToolKit.ts
@@ -0,0 +1,37 @@
+import { StructuredTool, type StructuredToolInterface } from '@langchain/core/tools'
+import consola from 'consola'
+import { z } from 'zod'
+import { GeocodeToolKit } from './GeocodeToolKit'
+
+const runtimeConfig = useRuntimeConfig()
+
+class WeatherSearchTool extends StructuredTool {
+  name = 'weatherForecastTool'
+  description = 'Use to forecast the weather given Latitude and Longitude'
+  schema = z.object({
+    lat: z.number().describe('The Latitude in decimal degree of the location to get forecast for'),
+    long: z.number().describe('The Longitude in decimal degree of the location to get forecast for'),
+  })
+
+  protected async _call(input: { lat: number, long: number }) {
+    const { lat, long } = input
+    consola.info('weatherForecastTool called!!!!')
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${runtimeConfig.weatherAPIKey}&q=${lat},${long}&days=7&aqi=no&alerts=no`
+    consola.info('url', url)
+    const forecast = await $fetch(url)
+    // comes back as object, JSON.stringify it since it will be stored in ToolMessage.content
+    return JSON.stringify(forecast)
+  }
+}
+
+export class WeatherToolKit extends GeocodeToolKit {
+  tools: StructuredToolInterface[]
+
+  constructor() {
+    super()
+    this.tools = [
+      ...super.getTools(),
+      new WeatherSearchTool(),
+    ]
+  }
+}
