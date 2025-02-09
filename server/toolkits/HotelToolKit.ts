2a7a5de1b7f3a5d2e5bb0e551066290137402a78
mikechao <mike.chao.one@gmail.com>
2025-02-09 09:19:12 -0800

    Creating HotelToolKit with HotelSearchTool

---
+++ server/toolkits/HotelToolKit.ts
@@ -0,0 +1,57 @@
+import { StructuredTool } from '@langchain/core/tools'
+import consola from 'consola'
+import { z } from 'zod'
+import { GeocodeToolKit } from './GeocodeToolKit'
+
+const runtimeConfig = useRuntimeConfig()
+
+interface LocationSearchResult {
+  data: any[]
+  error: any
+}
+
+class HotelSearchTool extends StructuredTool {
+  name = 'hotelSearchTool'
+  description = 'Used to search for hotels given Latitude and Longitude and location'
+  schema = z.object({
+    lat: z.number().describe('The Latitude in decimal degree of the location to search for hotels'),
+    long: z.number().describe('The Longitude in decimal degree of the location to search for hotels'),
+    searchQuery: z.string().describe('Text to use for searching based on the name of the location'),
+    radius: z.number().optional().default(10).describe(`Length of the radius in miles from the provided lat, long pair to filter results.`),
+  })
+
+  protected async _call({ lat, long, searchQuery, radius = 10 }: { lat: number, long: number, searchQuery: string, radius?: number }) {
+    consola.debug({ tag: 'hotelSearchTool', message: `hotelSearchTool called with lat: ${lat}, long: ${long}, searchQuery: ${searchQuery}, radius: ${radius}` })
+    const locationSearchURL = new URL('https://api.content.tripadvisor.com/api/v1/location/search')
+    locationSearchURL.searchParams.set('key', `${runtimeConfig.tripAdvisorAPIKey}`)
+    locationSearchURL.searchParams.set('searchQuery', searchQuery)
+    locationSearchURL.searchParams.set('category', 'hotels')
+    locationSearchURL.searchParams.set('latLong', `${lat},${long}`)
+    locationSearchURL.searchParams.set('radius', radius.toString())
+    locationSearchURL.searchParams.set('radiusUnit', 'mi')
+    consola.debug({ tag: 'hotelSearchTool', message: `locationSearchURL ${locationSearchURL.toString()}` })
+    // https://tripadvisor-content-api.readme.io/reference/searchforlocations
+    const { data, error } = await $fetch<LocationSearchResult>(locationSearchURL.toString(), {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    })
+    if (error) {
+      consola.error('error fetching location data', error)
+      // need to figure out how to handle errors in tools for langchain
+    }
+    if (!data.length) {
+      // not sure if this the right way to handle this
+      return 'No hotels found'
+    }
+    return JSON.stringify(data)
+  }
+}
+
+export class HotelToolKit extends GeocodeToolKit {
+  constructor() {
+    super()
+    this.tools.push(new HotelSearchTool())
+  }
+}
