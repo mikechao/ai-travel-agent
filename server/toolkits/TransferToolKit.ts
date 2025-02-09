bb190776fac0764253e4b6f34c42fc5a8d7183b4
mikechao <mike.chao.one@gmail.com>
2025-02-09 11:49:40 -0800

    Use a const object to hold transfers tool names

--- server/toolkits/TransferToolKit.ts
+++ server/toolkits/TransferToolKit.ts
@@ -4,54 +4,61 @@ import consola from 'consola'
 import { z } from 'zod'
 import { NodeNames } from '~/types/enums'
 
+export const TransferToolNames = {
+  HotelTransfer: 'hotelAdvisorTransfer',
+  TravelTransfer: 'travelAdvisorTransfer',
+  WeatherTransfer: 'weatherAdvisorTransfer',
+  HumanTransfer: 'humanTransfer',
+} as const
+
 class TransferToWeatherAdvisor extends StructuredTool {
-  name = 'weatherAdvisorTransfer'
+  name = TransferToolNames.WeatherTransfer
   description = 'Provides weather forecasts and clothing to pack advice by transferring to the \'weatherAdvisor\' named Petey the Pirate'
   schema = z.object({
     agent: z.any(),
   })
 
   protected async _call(input: { agent: any }) {
-    consola.debug({ tag: 'weatherAdvisorTransfer', message: `called with ${JSON.stringify(input.agent)}` })
+    consola.debug({ tag: TransferToolNames.WeatherTransfer, message: `called with ${JSON.stringify(input.agent)}` })
     return NodeNames.WeatherAdvisor
   }
 }
 
 class TransferToTravelAdvisor extends StructuredTool {
-  name = 'travelAdvisorTransfer'
+  name = TransferToolNames.TravelTransfer
   description = `Provides travel destinations recommendations by transferring to the agent \'travelAdvisor\' named Pluto the Pup`
   schema = z.object({
     agent: z.any(),
   })
 
   protected async _call(input: { agent: any }) {
-    consola.debug({ tag: 'travelAdvisorTransfer', message: `called with ${JSON.stringify(input.agent)}` })
+    consola.debug({ tag: TransferToolNames.TravelTransfer, message: `called with ${JSON.stringify(input.agent)}` })
     return NodeNames.TravelAdvisor
   }
 }
 
 class TransferToHotelAdvisor extends StructuredTool {
-  name = 'hotelAdvisorTransfer'
+  name = TransferToolNames.HotelTransfer
   description = `Provides hotel recommendations, search and details by transferring to the agent \'${NodeNames.HotelAdvisor}' named Penny Restmore`
   schema = z.object({
     agent: z.any(),
   })
 
   protected async _call(input: { agent: any }) {
-    consola.debug({ tag: 'hotelAdvisorTransfer', message: `called with ${JSON.stringify(input.agent)}` })
+    consola.debug({ tag: TransferToolNames.HotelTransfer, message: `called with ${JSON.stringify(input.agent)}` })
     return NodeNames.HotelAdvisor
   }
 }
 
 class TransferToHuman extends StructuredTool {
-  name = 'humanTransfer'
+  name = TransferToolNames.HumanTransfer
   description = `Gets input from the user, human, by transferring to the \'${NodeNames.HumanNode}\' node`
   schema = z.object({
     args: z.any(),
   })
 
   protected async _call(input: { args: any }) {
-    consola.debug({ tags: 'humanTransfer', message: `called with ${JSON.stringify(input)}` })
+    consola.debug({ tags: TransferToolNames.HumanTransfer, message: `called with ${JSON.stringify(input)}` })
     return NodeNames.HumanNode
   }
 }
