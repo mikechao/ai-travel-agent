75ed90260fe8daf67ff454b80c70212b86268645
mikechao <mike.chao.one@gmail.com>
2025-02-09 09:19:27 -0800

    Added TransferToHotelAdvisor tool

--- server/toolkits/TransferTools.ts
+++ server/toolkits/TransferTools.ts
@@ -29,21 +29,44 @@ class TransferToTravelAdvisor extends StructuredTool {
   }
 }
 
+class TransferToHotelAdvisor extends StructuredTool {
+  name = 'hotelAdvisorTransfer'
+  description = `Provides hotel recommendations, search and details by transferring to the agent \'${NodeNames.HotelAdvisor}' named Penny Restmore`
+  schema = z.object({
+    agent: z.any(),
+  })
+
+  protected async _call(input: { agent: any }) {
+    consola.debug({ tag: 'hotelAdvisorTransfer', message: `called with ${JSON.stringify(input.agent)}` })
+    return `Successfully transferred to ${input.agent}`
+  }
+}
+
 export class TransferTools {
   transferToWeatherAdvisor: StructuredTool
   transferToTravelAdvisor: StructuredTool
+  transferToHotelAdvisor: StructuredTool
   transferToolsByName: Map<string, StructuredTool>
   transferLocationByToolName: Map<string, NodeNames>
 
   constructor() {
     this.transferToWeatherAdvisor = new TransferToWeatherAdvisor()
     this.transferToTravelAdvisor = new TransferToTravelAdvisor()
+    this.transferToHotelAdvisor = new TransferToHotelAdvisor()
+
     this.transferToolsByName = new Map<string, StructuredTool>()
     this.transferToolsByName.set(this.transferToWeatherAdvisor.name, this.transferToWeatherAdvisor)
     this.transferToolsByName.set(this.transferToTravelAdvisor.name, this.transferToTravelAdvisor)
+    this.transferToolsByName.set(this.transferToHotelAdvisor.name, this.transferToHotelAdvisor)
+
     this.transferLocationByToolName = new Map<string, NodeNames>()
     this.transferLocationByToolName.set(this.transferToWeatherAdvisor.name, NodeNames.WeatherAdvisor)
     this.transferLocationByToolName.set(this.transferToTravelAdvisor.name, NodeNames.TravelAdvisor)
+    this.transferLocationByToolName.set(this.transferToHotelAdvisor.name, NodeNames.HotelAdvisor)
+  }
+
+  getToolsForHotelAdvisor(): StructuredTool[] {
+    return [this.transferToWeatherAdvisor, this.transferToTravelAdvisor]
   }
 
   getTransferToWeatherAdvisor() {
