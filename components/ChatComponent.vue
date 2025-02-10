ccf8914827ab8f115a0fced199dd80047efeb953
mikechao <mike.chao.one@gmail.com>
2025-02-10 11:14:01 -0800

    Open generated links in new tab

--- components/ChatComponent.vue
+++ components/ChatComponent.vue
@@ -69,6 +69,12 @@ const md = new MarkdownIt({
 md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
   // Add Tailwind CSS classes to style links
   tokens[idx].attrPush(['class', 'p-button-link'])
+  // open links in new tab
+  tokens[idx].attrPush(['target', '_blank'])
+  // noopener to prevent new page accessing originating window
+  // noreferrer do not send refer header, protect privacy of user
+  // and prevent destination site from knowing where the visitor came from
+  tokens[idx].attrPush(['rel', 'noopener noreferrer'])
   return self.renderToken(tokens, idx, options)
 }
 
